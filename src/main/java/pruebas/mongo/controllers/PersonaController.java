0c5352bf92309fc28da5db10ae32fa1ebb555e75
Lucero Raul <luceroraul12@gmail.com>
2022-10-04 10:15:47 -0300

    Configurado y listo para usar, la configuraci[on es similar a SQL, solo que previamente hay que crear la base de datos en MongoCompass. La diferencia que hay es que para mongo no te crea automaticamente la coleccion = tabla hasta el momento en que se hace un post request y ahi si. Despues de ese primer paso, todo es igual. Aunque podria a haberla creado previamente desde compass

--- src/main/java/pruebas/mongo/controllers/PersonaController.java
+++ src/main/java/pruebas/mongo/controllers/PersonaController.java
@@ -1,4 +1,30 @@
 package pruebas.mongo.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pruebas.mongo.entities.Persona;
+import pruebas.mongo.repositories.Personarepository;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/personas")
 public class PersonaController {
+
+    @Autowired
+    Personarepository personarepository;
+
+    @GetMapping
+    public List<Persona> obtenerPersonas(){
+        return personarepository.findAll();
+    }
+
+    @PostMapping
+    public String cargarPersona(){
+        personarepository.save(new Persona("Raul", 23423749));
+        return "Persona cargada";
+    }
 }
--- src/main/java/pruebas/mongo/entities/Persona.java
+++ src/main/java/pruebas/mongo/entities/Persona.java
@@ -2,11 +2,43 @@ package pruebas.mongo.entities;
 
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+@Document(collection = "pruebaaaa")
 public class Persona {
 
     @Id
     private String id;
     private String nombre;
     private Integer documento;
+
+    public Persona(String nombre, Integer documento) {
+        this.nombre = nombre;
+        this.documento = documento;
+    }
+
+
+    public String getId() {
+        return this.id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getDocumento() {
+        return this.documento;
+    }
+
+    public void setDocumento(Integer documento) {
+        this.documento = documento;
+    }
 }
